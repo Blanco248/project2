823d1d4cd165bac655785bfe07fc43b8f5d14fb8
Blanco <sebastiengeahchan@hotmail.com>
2023-08-27 19:21:42 -0400

    finally got tomake it interactive but i dont understand shit, ChatGPT blessed me

--- ressources/script.js
+++ ressources/script.js
@@ -1,5 +1,27 @@
-document.addEventListener("click", appear) ;
+document.addEventListener("DOMContentLoaded", function() {
+    const projectList = document.querySelector(".projects");
 
-function appear() {
-    document.getElementsByClassName("FP").style.display = "block"
-}
+    projectList.addEventListener("click", function(event) {
+        const clickedElement = event.target;
+
+        if (clickedElement.tagName === "A") {
+            const liElement = clickedElement.parentElement;
+            const className = liElement.id;
+            const img = projectList.querySelector(`img.${className}`);
+            const p = projectList.querySelector(`p.${className}`);
+
+            if (img && p) {
+                toggleVisibility(img);
+                toggleVisibility(p);
+            }
+        }
+    });
+
+    function toggleVisibility(element) {
+        if (element.style.display === "block") {
+            element.style.display = "none";
+        } else {
+            element.style.display = "block";
+        }
+    }
+});
