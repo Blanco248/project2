8a70835d12a309118a2f0eaa32a6144e4e691d77
Blanco <sebastiengeahchan@hotmail.com>
2023-08-29 16:18:33 -0400

    finalized project

--- ressources/script.js
+++ ressources/script.js
@@ -25,3 +25,19 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 });
+
+
+
+const timeContainer = document.getElementById("time-container");
+    const BIRTH_DAY = "3-31-2023";
+    const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
+    const intlNumberFormatter = new Intl.NumberFormat("en-US");
+
+    setInterval(() => {
+      const now = new Date();
+      const difference = Math.floor(
+        (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
+      );
+
+      timeContainer.innerText = intlNumberFormatter.format(difference);
+    }, 1000);
